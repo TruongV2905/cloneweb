d69ebeb0bc1a060eeb8cc430d5cb76d3e892d681
TruongV2905 <truong0345218921@gmail.com>
2024-04-02 10:13:02 +0700

    second

--- Js/carousel.js
+++ Js/carousel.js
@@ -12,3 +12,35 @@ $(".sliderShow-section").slick({
     return '<div class="dot"></div>';
   },
 });
+$(".thinkAbout__category").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 1,
+  prevArrow: false,
+  nextArrow: false,
+  zIndex: 1,
+  // adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+// $(".thinkAbout__category").on("swipe", function (event, slick, direction) {
+//   console.log(slick);
+// });
